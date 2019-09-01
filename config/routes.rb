9955a2008e50021332d92969b5ba808fbe28aef1
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-09-01 13:28:59 -0300

    removing blank spaces.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/mostrar-monstro/:id', to: "monsters#show", as: "show_monster"
   delete '/deletar-monstro/:id', to: "monsters#destroy", as: "destroy_monster"
   get '/editar-monstro/:id', to: "monsters#edit", as: "edit_monster"
-	put '/editar-monstro/:id', to: "monsters#update", as: "update_monster"
+  put '/editar-monstro/:id', to: "monsters#update", as: "update_monster"
   
   root to: 'monsters#index'
 end
